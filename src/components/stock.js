e90508c80ab1b4c52f4490f8519ab858377f319e
llleeeaaannn <piercefrank10@gmail.com>
2023-03-25 16:03:21 +0000

    Styled slider and added calculcation for current price and percent

--- src/components/stock.js
+++ src/components/stock.js
@@ -1,24 +1,42 @@
-// import styles from '../styles/navbar.module.css';
+import styles from '../styles/stock.module.css';
 import { useState } from 'react';
 import Slider from 'react-input-slider';
 
 const Stock = ({ ticker }) => {
 
-  const [stockNewPrice, setStockNewPrice] = useState(50);
+  const stockPrice = 50;
+  const [stockNewPrice, setStockNewPrice] = useState(stockPrice);
+
+  const sliderStyles = {
+    track: {
+      backgroundColor: 'blue'
+    },
+    active: {
+      backgroundColor: 'red'
+    },
+    thumb: {
+      width: 20,
+      height: 20
+    },
+    disabled: {
+      opacity: 0.5
+    }
+  }
 
   return (
-    <div>
+    <div className={styles.stock}>
 
-      <div>
+      <div className={styles.overview}>
         <span>AAPL</span>
-        <span>Apple</span>
+        <span>Apple Inc</span>
         <div>
           <span>$153.80</span>
           <span>+1.08%</span>
         </div>
       </div>
 
-      <div>
+      <div className={styles.slider}>
+        <span>${ stockNewPrice }</span>
         <Slider
           axis="x"
           xstep={0.01}
@@ -26,10 +44,12 @@ const Stock = ({ ticker }) => {
           xmax={100}
           x={stockNewPrice}
           onChange={({ x }) => setStockNewPrice(x.toFixed(2))}
+          styles={sliderStyles}
         />
+        <span>{ (stockNewPrice / stockPrice * 100 - 100).toFixed(2) }%</span>
       </div>
 
-      <div>
+      <div className={styles.updated}>
         <div>
           <button>%</button>
           <button>$</button>
