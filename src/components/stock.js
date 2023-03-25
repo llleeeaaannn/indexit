8b775eacc54c6c434be7040825000d0cd88eba08
llleeeaaannn <piercefrank10@gmail.com>
2023-03-25 14:18:42 +0000

    Removed react-slider dependency and component

--- src/components/stock.js
+++ src/components/stock.js
@@ -1,5 +1,4 @@
 // import styles from '../styles/navbar.module.css';
-import ReactSlider from 'react-slider';
 
 const Stock = ({ ticker }) => {
 
@@ -16,12 +15,7 @@ const Stock = ({ ticker }) => {
       </div>
 
       <div>
-        <ReactSlider
-          className="horizontal-slider"
-          thumbClassName="example-thumb"
-          trackClassName="example-track"
-          renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
-        />
+
       </div>
 
     </div>
