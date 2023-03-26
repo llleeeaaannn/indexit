57642400cdc2df0531fa4e288f8ed5db8a1f6cd3
llleeeaaannn <piercefrank10@gmail.com>
2023-03-26 20:37:02 +0100

    Removing unused imports

--- src/views/home.js
+++ src/views/home.js
@@ -1,9 +1,6 @@
-import { useState, useEffect, useContext } from 'react';
-import { AppContext } from '../App';
 import Stock from '../components/stock';
 import Test from '../components/test';
 import stocksData from '../data/stocks';
-
 
 const Home = () => {
 
