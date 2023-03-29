6dc9abfd1a605484679ceb35b3ae09b844868858
llleeeaaannn <piercefrank10@gmail.com>
2023-03-29 22:48:38 +0100

    Began global styling

--- src/views/home.js
+++ src/views/home.js
@@ -1,3 +1,4 @@
+import styles from '../styles/home.module.css';
 import Stock from '../components/stock';
 import Test from '../components/test';
 import stocksData from '../data/stocks';
@@ -5,7 +6,7 @@ import stocksData from '../data/stocks';
 const Home = () => {
 
   return (
-    <div>
+    <div className={styles.home}>
       {
         Object.keys(stocksData).map((share, i) => (
           <Stock share={stocksData[share]} key={i} />
