d8233d9d436a2ef145d3256f150664dbd9aa2aed
llleeeaaannn <piercefrank10@gmail.com>
2023-03-31 15:30:41 +0100

    Added function to create state from stock data

--- src/views/home.js
+++ src/views/home.js
@@ -1,9 +1,21 @@
 import styles from '../styles/home.module.css';
 import Stock from '../components/stock';
 import Test from '../components/test';
-import stocksData from '../data/stocks';
+import stocksData from '../data/stocksdata';
 
 const Home = () => {
+
+  const createStockStates = (data) => {
+    const states = {};
+    for (let key in data) {
+      states[key] = {
+        ticker: data.key.ticker,
+        name: data.key.name,
+        weight: data.key.weight,
+        currentprice: data.key.currentprice
+      }
+    }
+  }
 
   return (
     <div className={styles.home}>
