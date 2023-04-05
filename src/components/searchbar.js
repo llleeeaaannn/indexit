6482afbc6a72402f870a21605dddf41b4675c8fb
llleeeaaannn <piercefrank10@gmail.com>
2023-04-05 22:11:51 +0100

    Created searchbar element outline and state, not functioning

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,9 +1,19 @@
 import styles from '../styles/searchbar.module.css';
+import { useState } from 'react';
 
 const Searchbar = ({  }) => {
 
+  const [value, setValue] = useState();
+
   return (
     <div className={styles.searchbar}>
+
+      <form>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+      </form>
 
     </div>
   )
