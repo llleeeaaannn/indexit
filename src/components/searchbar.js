06987280261004102b25c0e80c88ce59ace60e52
llleeeaaannn <piercefrank10@gmail.com>
2023-04-05 22:03:27 +0100

    Created searchbar component

---
+++ src/components/searchbar.js
@@ -0,0 +1,14 @@
+import styles from '../styles/stock.module.css';
+import { useState, useEffect } from 'react';
+import Slider from 'react-input-slider';
+
+const Searchbar = ({  }) => {
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Searchbar;
