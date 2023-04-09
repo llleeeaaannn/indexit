07ddc53aabe3576b382793faca3a46d8e2c2227f
llleeeaaannn <piercefrank10@gmail.com>
2023-04-09 01:53:38 +0100

    Removed unusued state and refactored code

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -5,19 +5,17 @@ const Searchbar = ({ tickers, setDisplayStocks }) => {
 
   const [value, setValue] = useState('');
 
-  const [results, setResults] = useState();
-
   // Convert input to uppercase, ensure it is truthy, search against all tickers, set displayStocks state to matching tickers
   const getResults = () => {
-    let input = value.trim().toUpperCase();
-    let inputLength = input.length;
+    const input = value.trim().toUpperCase();
+    const inputLength = input.length;
 
     if (inputLength === 0) {
       setDisplayStocks(tickers);
       return;
     }
 
-    let matches = tickers.filter(ticker => ticker.toUpperCase().slice(0, inputLength) === input);
+    const matches = tickers.filter(ticker => ticker.toUpperCase().slice(0, inputLength) === input);
     setDisplayStocks(matches);
   }
 
