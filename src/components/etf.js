7b960ecdf6669a25c002ef24319d2838aa2f5f38
Frank Pierce <piercefrank10@gmail.com>
2023-05-03 08:53:38 +0000

    ETF layout idea

--- src/components/etf.js
+++ src/components/etf.js
@@ -4,6 +4,27 @@ const Etf = () => {
 
   return (
     <div>
+        <div>
+            <div className={styles.details}>
+
+                <span>Ticker</span>
+                <span>Name</span>
+
+            </div>
+
+            <div className={styles.price}>
+
+                <div>
+                    <span>Price</span>
+                    <span>New Price</span>
+                </div>
+                <div>
+                    <span>Percent Change</span>
+                    <span>Dollar Change</span>
+                </div>
+
+            </div>
+        </div>
     </div>
   )
 }
