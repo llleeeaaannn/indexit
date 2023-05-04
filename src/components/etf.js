1402ac1e6030d30ef9c04c695e1b378b38f5c7ea
Frank Pierce <piercefrank10@gmail.com>
2023-05-04 16:19:34 +0000

    ETF component styling

--- src/components/etf.js
+++ src/components/etf.js
@@ -5,13 +5,13 @@ const Etf = () => {
   return (
     <div className={styles.etf}>
 
-      <div>
+      <div className={styles.overview}>
         <h3>Ticker</h3>
         <h4>Name</h4>
         <span>$Original Price</span>
       </div>
 
-      <div>
+      <div className={styles.prices}>
         <span>$UpdatedPrice</span>
         <span>PercentChange%</span>
       </div>
